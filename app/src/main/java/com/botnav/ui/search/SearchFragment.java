36b206759d3abcc8d0da2514da6d85f9b7bda3c5
zuhdih <71930797+zuhdih@users.noreply.github.com>
2022-01-04 08:32:56 +0700

    Commit

--- app/src/main/java/com/botnav/ui/search/SearchFragment.java
+++ app/src/main/java/com/botnav/ui/search/SearchFragment.java
@@ -1,9 +1,16 @@
 package com.botnav.ui.search;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -12,12 +19,26 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.botnav.R;
 import com.botnav.databinding.FragmentSearchBinding;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class SearchFragment extends Fragment {
 
     private SearchViewModel searchViewModel;
     private FragmentSearchBinding binding;
+
+    ImageView rando;
+    Button btnRand;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -35,6 +56,92 @@ public class SearchFragment extends Fragment {
             }
         });
         return root;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+
+        rando= view.findViewById(R.id.imageRand);
+        btnRand = view.findViewById(R.id.buttonSearchRand);
+
+        btnRand.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               new FetchImage().start();
+            }
+        });
+
+
+    }
+
+    class FetchImage extends Thread{
+
+        Bitmap bitmap;
+        String data = "";
+        ProgressDialog progressDialog;
+
+        FetchImage(){
+
+
+
+        }
+
+        @Override
+        public void run() {
+
+            getActivity().runOnUiThread(new Runnable() {
+
+                @Override
+                public void run() {
+
+                    progressDialog = new ProgressDialog(getActivity());
+                    progressDialog.setMessage("Getting your pic....");
+                    progressDialog.setCancelable(false);
+                    progressDialog.show();
+
+                }
+            });
+
+            try {
+                URL url =  new URL("https://api.thecatapi.com/v1/images/search");
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                InputStream inputStream = httpURLConnection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                String line;
+
+                while ((line = bufferedReader.readLine())!= null){
+                    data = data + line;
+
+                }
+
+                if (!data.isEmpty()){
+                    JSONArray arr = new JSONArray(data);
+                    JSONObject obj = arr.getJSONObject(0);
+                    String img = obj.getString("url");
+                    InputStream i_s = new URL(img).openStream();
+                    bitmap = BitmapFactory.decodeStream(i_s);
+
+
+                }
+            }
+            catch (Exception e){
+                e.printStackTrace();
+            }
+
+            getActivity().runOnUiThread(new Runnable() {
+
+                @Override
+                public void run() {
+                    if (progressDialog.isShowing())
+                        progressDialog.dismiss();
+                    rando.setImageBitmap(bitmap);
+
+                }
+            });
+
+
+
+        }
     }
 
     @Override
--- app/src/main/java/com/botnav/ui/upload/UploadFragment.java
+++ app/src/main/java/com/botnav/ui/upload/UploadFragment.java
@@ -1,23 +1,46 @@
 package com.botnav.ui.upload;
 
+import static android.app.Activity.RESULT_CANCELED;
+import static android.app.Activity.RESULT_OK;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.botnav.MainActivity;
+import com.botnav.R;
 import com.botnav.databinding.FragmentUploadBinding;
+import com.github.drjacky.imagepicker.ImagePicker;
 
 public class UploadFragment extends Fragment {
 
     private UploadViewModel uploadViewModel;
     private FragmentUploadBinding binding;
+
+    ImageView uploaded;
+    Button btnOpen;
+    Button btnUp;
+    Bitmap upImage;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -28,16 +51,84 @@ public class UploadFragment extends Fragment {
         View root = binding.getRoot();
 
         final TextView textView = binding.textUpload;
+        //final View view = inflater.inflate(R.layout.fragment_upload,container,false);
+
+
         uploadViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
             }
         });
+
         return root;
+
+
+
+    }
+
+   @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+
+        btnOpen = view.findViewById(R.id.buttonUploadOpen);
+        btnUp = view.findViewById(R.id.buttonUploadUpload);
+
+        btnOpen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent pickPhoto = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(pickPhoto , 1);
+            }
+        });
+
+        btnUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(upImage != null){
+
+                }
+                else{
+
+                }
+            }
+        });
+
+
     }
 
     @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        uploaded = (ImageView) getView().findViewById(R.id.imageUp);
+        if (resultCode != RESULT_CANCELED) {
+            switch (requestCode) {
+                case 0:
+                    if (resultCode == RESULT_OK && data != null) {
+                        Bitmap selectedImage = (Bitmap) data.getExtras().get("data");
+                        upImage= selectedImage;
+                        uploaded.setImageBitmap(selectedImage);
+                    }
+                    break;
+                case 1:
+                    if (resultCode == RESULT_OK && data != null) {
+                        Uri selectedImage = data.getData();
+                        String[] filePathColumn = {MediaStore.Images.Media.DATA};
+                        if (selectedImage != null) {
+                            Cursor cursor = getActivity().getContentResolver().query(selectedImage, filePathColumn, null, null, null);
+                            if (cursor != null) {
+                                cursor.moveToFirst();
+                                int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                                String picturePath = cursor.getString(columnIndex);
+                                upImage = BitmapFactory.decodeFile(picturePath);
+                                uploaded.setImageBitmap(upImage);
+                                cursor.close();
+                            }
+                        }                    }
+                    break;
+            }}}
+
+
+            @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
